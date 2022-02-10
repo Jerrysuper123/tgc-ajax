ae7bfa31f94987888bdc6faab2da3dedd39f8baf
Jerry Chen <jerryq.chen@gmail.com>
2022-02-10 16:42:54 +0800

    completed q6

--- hands-on/q6-simple-api/script.js
+++ hands-on/q6-simple-api/script.js
@@ -1,2 +1,10 @@
+let url = "https://programming-quotes-api.herokuapp.com/quotes/random";
 
-let url = "https://programming-quotes-api.herokuapp.com/quotes/random"
+document
+  .querySelector("#loadButton")
+  .addEventListener("click", async function () {
+    let response = await axios.get(url);
+    // console.log(response.data.en);
+    let content = document.querySelector("#quote");
+    content.innerHTML = response.data.en;
+  });
